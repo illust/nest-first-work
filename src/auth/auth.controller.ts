170d2b425bd17d5f8355507f22f4b5a9b249e7c0
svenwong <illust0130@foxmail.com>
2024-04-02 11:38:12 +0800

    add guard decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import { AuthDto } from './dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
